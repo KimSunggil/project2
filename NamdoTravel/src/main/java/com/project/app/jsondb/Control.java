d50f3fa6db927d93c6202e53f43207b63b46d47a
enls <66344370+KimSunggil@users.noreply.github.com>
2020-10-08 17:50:46 +0900

    ㅇㅇ

--- NamdoTravel/src/main/java/com/project/app/jsondb/Control.java
+++ NamdoTravel/src/main/java/com/project/app/jsondb/Control.java
@@ -5,7 +5,7 @@ public class Control {
 		// 동내 기상 JSON데이터를 얻기위해 필요한 요청변수를 입력해줍니다.
 		// baseDate : 기준날짜, baseTime : 기준시간, x : 경도, y : 위도 참고문서를 확인하세요
 		String baseDate = "20201008";
-		String baseTime = "1100"; // 0200, 0500, 0800, 1100, 1400, 2000, 2300
+		String baseTime = "0500"; // 0200, 0500, 0800, 1100, 1400, 2000, 2300
 		int x = 92;
 		int y = 131;
 
